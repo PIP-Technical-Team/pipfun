88e81ef13637833d8a60da9076fd48bb8fee31f3
R.Andres Castaneda <acastanedaa@worldbank.org>
2022-08-08 15:23:51 -0400

    update readme

--- README.md
+++ README.md
@@ -14,11 +14,11 @@ You can install the development version of pipfun from
 [GitHub](https://github.com/) with:
 
 ``` r
-# install.packages("devtools")
-devtools::install_github("PIP-Technical-Team/pipfun")
+# install.packages("remotes")
+remotes::install_github("PIP-Technical-Team/pipfun")
 ```
 
-## Example
+## Examples
 
 This is a basic example which shows you how to solve a common problem:
 
@@ -27,29 +27,61 @@ library(pipfun)
 ## basic example code
 ```
 
-What is special about using `README.Rmd` instead of just `README.md`?
-You can include R chunks like so:
+### Saving files
+
+Save a list
 
 ``` r
-summary(cars)
-#>      speed           dist       
-#>  Min.   : 4.0   Min.   :  2.00  
-#>  1st Qu.:12.0   1st Qu.: 26.00  
-#>  Median :15.0   Median : 36.00  
-#>  Mean   :15.4   Mean   : 42.98  
-#>  3rd Qu.:19.0   3rd Qu.: 56.00  
-#>  Max.   :25.0   Max.   :120.00
+
+  tdir <- fs::path_temp("pipfun-l")
+
+  lx <- list(x = 1)
+  measure <- "ltst"
+
+  pip_sign_save(x = lx,
+              measure = measure,
+              msrdir = tdir,
+              save_dta = TRUE)
+#> ! Data signature has changed
+#> 'ltst.rds' has been updated
+  
+  fs::dir_tree(tdir)
+#> C:/Users/wb384996/AppData/Local/Temp/Rtmp6ZvPdM/pipfun-l
+#> +-- ltst.qs
+#> +-- ltst.rds
+#> +-- ltst_datasignature.txt
+#> \-- _vintage
+#>     +-- ltst_20220808152338.qs
+#>     \-- ltst_20220808152338.rds
+  fs::dir_delete(tdir)
 ```
 
-You’ll still need to render `README.Rmd` regularly, to keep `README.md`
-up-to-date. `devtools::build_readme()` is handy for this. You could also
-use GitHub Actions to re-render `README.Rmd` every time you push. An
-example workflow can be found here:
-<https://github.com/r-lib/actions/tree/v1/examples>.
+Save a data-frame
 
-You can also embed plots, for example:
+``` r
 
-<img src="man/figures/README-pressure-1.png" width="100%" />
+  tdir <- fs::path_temp("pipfun-df")
 
-In that case, don’t forget to commit and push the resulting figure
-files, so they display on GitHub and CRAN.
+  lx <- data.frame(x = 1)
+  measure <- "ltst"
+
+  pip_sign_save(x = lx,
+              measure = measure,
+              msrdir = tdir,
+              save_dta = TRUE)
+#> Warning: package 'fstcore' was built under R version 4.1.3
+#> ! Data signature has changed
+#> 'ltst.fst' has been updated
+  
+  fs::dir_tree(tdir)
+#> C:/Users/wb384996/AppData/Local/Temp/Rtmp6ZvPdM/pipfun-df
+#> +-- ltst.dta
+#> +-- ltst.fst
+#> +-- ltst.qs
+#> +-- ltst_datasignature.txt
+#> \-- _vintage
+#>     +-- ltst_20220808152338.dta
+#>     +-- ltst_20220808152338.fst
+#>     \-- ltst_20220808152338.qs
+  fs::dir_delete(tdir)
+```
--- tests/testthat/test-pip_sign_save.R
+++ tests/testthat/test-pip_sign_save.R
@@ -1,3 +1,125 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
-})
+
+
+test_that("Saves correctly if object is a list", {
+
+  tdir <- fs::path_temp("pipfun-l")
+
+  lx <- list(x = 1)
+  measure <- "ltst"
+
+  saved <-
+    pip_sign_save(x = lx,
+                measure = measure,
+                msrdir = tdir,
+                save_dta = TRUE)
+
+  expect_true(saved)
+
+
+  fs::path(tdir, "_vintage") |>
+    fs::dir_exists() |>
+    expect_true(label = "Vintage folder is created")
+
+
+
+  fs::path(tdir, "_vintage") |>
+    fs::dir_exists() |>
+    expect_true(label = "Vintage folder is created")
+
+
+
+  fs::path(tdir, measure, ext = "qs") |>
+    fs::file_exists() |>
+    expect_true(label = "qs file is created from list")
+
+
+  fs::path(tdir, measure, ext = "rds") |>
+    fs::file_exists() |>
+    expect_true(label = "rds file is created from list")
+
+  fs::path(tdir, measure, ext = "dta") |>
+    fs::file_exists() |>
+    expect_false(label = "No dta file should be saved")
+
+
+  fs::path(tdir, glue("{measure}_datasignature.txt")) |>
+    fs::file_exists() |>
+    expect_true(label = "Data signature created")
+
+
+  # Since the file has not changed, we should expect false if saved again.
+  saved2 <-
+    pip_sign_save(x = lx,
+                  measure = measure,
+                  msrdir = tdir,
+                  save_dta = TRUE)
+
+  expect_false(saved2)
+  fs::dir_delete(tdir)
+
+}
+)
+
+test_that("Saves correctly if object is a dataframe", {
+
+  tdir <- fs::path_temp("pipfun-df")
+
+  lx <- data.frame(x = 1)
+  measure <- "dftest"
+
+  saved <-
+    pip_sign_save(x = lx,
+                measure = measure,
+                msrdir = tdir,
+                save_dta = TRUE) |>
+    suppressWarnings()
+
+  expect_true(saved)
+
+
+  fs::path(tdir, "_vintage") |>
+    fs::dir_exists() |>
+    expect_true(label = "Vintage folder is created")
+
+
+
+  fs::path(tdir, "_vintage") |>
+    fs::dir_exists() |>
+    expect_true(label = "Vintage folder is created")
+
+
+
+  fs::path(tdir, measure, ext = "qs") |>
+    fs::file_exists() |>
+    expect_true(label = "qs file is created from list")
+
+
+  fs::path(tdir, measure, ext = "rds") |>
+    fs::file_exists() |>
+    expect_false(label = "IF dataframe, rds should not be created")
+
+  fs::path(tdir, measure, ext = "dta") |>
+    fs::file_exists() |>
+    expect_true(label = "If object is data frame, dta should be saved")
+
+  fs::path(tdir, measure, ext = "fst") |>
+    fs::file_exists() |>
+    expect_true(label = "if dataframe, fst should be created")
+
+
+  fs::path(tdir, glue("{measure}_datasignature.txt")) |>
+    fs::file_exists() |>
+    expect_true(label = "Data signature created")
+
+
+  # Since the file has not changed, we should expect false if saved again.
+  saved2 <-
+    pip_sign_save(x = lx,
+                  measure = measure,
+                  msrdir = tdir,
+                  save_dta = TRUE)
+
+  expect_false(saved2)
+
+}
+)
