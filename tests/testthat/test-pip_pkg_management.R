49c0cec973970773d652d1b8bbc56550e6fcf358
R.Andres Castaneda Aguilar <acastanedaa@worldbank.org>
2024-06-25 15:32:39 -0400

    add tests

--- tests/testthat/test-pip_pkg_management.R
+++ tests/testthat/test-pip_pkg_management.R
@@ -1,3 +1,52 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+# Test for is_package_loaded
+test_that("is_package_loaded returns TRUE for loaded packages", {
+  expect_true(is_package_loaded("base")) # base should always be loaded
+})
+
+test_that("is_package_loaded returns FALSE for not loaded packages", {
+  expect_false(is_package_loaded("thisPackageShouldNotExistHopefully"))
+})
+
+# Test for is_package_attached
+test_that("is_package_attached returns TRUE for attached packages", {
+  # Assuming fst is attached for this test
+  pkg <- "fst"
+  expect_false(is_package_attached(pkg))
+
+  library(pkg, character.only = TRUE)
+  expect_true(is_package_attached(pkg))
+  detach(paste0("package:", pkg), unload = TRUE, character.only = TRUE)
+})
+
+test_that("is_package_attached returns FALSE for not attached packages", {
+  expect_false(is_package_attached("thisPackageShouldNotExistHopefully"))
+})
+
+
+test_that("check_pkg_active aborts for inactive packages", {
+  expect_error(check_pkg_active("thisPackageShouldNotExistHopefully"))
+})
+
+# Test for is_package_attached
+test_that("Attached and loaded as expected", {
+  # data.table is fully imported by pipfun
+  pkg <- "data.table"
+  expect_true(is_package_loaded(pkg))
+
+  # yet it is not attached
+  expect_false(is_package_attached(pkg))
+
+  # now we attach it
+  library(pkg, character.only = TRUE)
+  expect_true(is_package_attached(pkg))
+
+  # And detach it again
+
+  unloadNamespace(paste0("package:", pkg))
+
+  # it is not attached anymore
+  expect_false(is_package_attached(pkg))
+
+  # but it is loaded
+  expect_true(is_package_loaded(pkg))
 })
