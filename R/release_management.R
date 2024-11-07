a3728817ec66a670b56593c49003292b48fb1883
R.Andres Castaneda Aguilar <acastanedaa@worldbank.org>
2024-11-07 17:25:09 -0500

    update vignnette and document

--- R/release_management.R
+++ R/release_management.R
@@ -127,7 +127,7 @@ create_aux_dir <- function(aux_versions,
                            ) {
 
   wdir    <- fs::path(working_dir, "aux_data")
-  newdirs <- create_dir(wdir, ndirs = aux_versions)
+  newdirs <- create_dir(wdir, dirs = aux_versions)
 
   return(newdirs)
 
@@ -144,13 +144,14 @@ create_aux_dir <- function(aux_versions,
 #'
 #' @rdname create_dir
 #' @keywords internal
-create_pc_dir <- function(pc_versions,
-                          working_dir = fs::path(Sys.getenv("PIP_ROOT_DIR"),
-                                                 getOption("pipfun.working_dir"))
+create_pc_dir <-
+  function(pc_versions,
+           working_dir = fs::path(Sys.getenv("PIP_ROOT_DIR"),
+                                  getOption("pipfun.working_dir"))
                           ) {
 
   wdir    <- fs::path(working_dir, "pc_data")
-  newdirs <- create_dir(wdir, ndirs = pc_versions)
+  newdirs <- create_dir(wdir, dirs = pc_versions)
 
   return(newdirs)
 
@@ -160,15 +161,15 @@ create_pc_dir <- function(pc_versions,
 #' Create directories in folder
 #'
 #' @param wdir chracter: working directory path
-#' @param ndirs chracter: new directories name that will be created inside
+#' @param dirs chracter: directories to be created inside or removed from
 #'   `wdir`
 #'
 #' @return logical vector. the names of the elements correspond to the directory
 #'   paths
 #' @export
-create_dir <- function(wdir, ndirs,
+create_dir <- function(wdir, dirs,
                        verbose = getOption("pipfun.verbose")) {
-  dir_ex <- fs::path(wdir, ndirs) |>
+  dir_ex <- fs::path(wdir, dirs) |>
     fs::dir_exists()
 
   if (all(dir_ex == TRUE)) {
@@ -177,26 +178,190 @@ create_dir <- function(wdir, ndirs,
     return(invisible(dir_ex))
   }
 
-  ndirs <- dir_ex[dir_ex == FALSE]
-  ndirs_ex <- names(ndirs) |>
+  dirs <- dir_ex[dir_ex == FALSE]
+  dirs_ex <- names(dirs) |>
     fs::dir_create(recurse = TRUE) |>
     fs::dir_exists()
 
-  return(ndirs_ex)
+  return(dirs_ex)
 }
 
 
-remove_pip_release <- function() {
+
+remove_pip_release <-
+  function(release,
+           identity    = c("PROD", "INT", "TEST"),
+           verbose     = getOption("pipfun.verbose"),
+           working_dir = NULL,
+           ppps        = getOption("pipfun.ppps"),
+           ...) {
+  # defenses ----------
+  identity <- match.arg(identity)
+  call_args <- all_args()
+  check_pip_release_inputs(call_args)
+
+  if (is.null(working_dir)) {
+    root_dir    = Sys.getenv("PIP_ROOT_DIR")
+    official_dir = fs::path(root_dir,
+                           getOption("pipfun.working_dir"))
+
+    selection <- menu(choices =
+                        c(official_dir,
+                          "select your own dir"),
+         title = "Select from where you want to delete the data")
+    if (selection == 1) {
+      working_dir <- official_dir
+    } else if (selection == 2) {
+      cli::cli_abort("implement browse or something like that... (DEVELPMENT) ")
+    }else {
+      cli::cli_abort("option not allowed")
+    }
+
+  } else {
+    end_of_dir <- fs::path_file(working_dir)
+    if (end_of_dir != getOption("pipfun.working_dir")) {
+      working_dir <- fs::path(working_dir, getOption("pipfun.working_dir"))
+    }
+  }
+
+  # add new release to pool --------
+  ## get current releases ---------
+  pr <- get_pip_releases(force = TRUE, ...)
+  # pr <- get_pip_releases(force = TRUE)
+  mt <- attr(pr, "metadata") # get metadata from GH
+
+
+  # find release to be removed ----
+  release2del  <- release
+  identity2del <- identity
+  filtered_pr  <- pr[release == release2del & identity == identity2del]
+  nr <- nrow(filtered_pr)
+
+  if (nr == 0) {
+    cli::cli_abort("Release {.field {release2del}_{identity2del}} does not exist")
+  } else if (nr > 1) {
+    cli::cli_abort("Release {.field {release2del}_{identity2del}} does
+                   uniquely identify the data.
+                   Check with
+                   {.run pipfun::get_pip_releases(force = TRUE)}")
+  }
+
+  # get PPP metadata
+  ppp <- get_latest_ppp_versions(ppps = ppps)
+  ppp[, n := .I]
+
+  # update release data
+  dt <-  pr[!filtered_pr, on = c("release", "identity")]
+
+  # remove folder for welfare data ----
+  ## get versions id -----------
+  # This part must be converted into a function because it is repeated in
+  # new_pip_releases() as well.
+  df <-
+    filtered_pr[
+      # expand to match ppp
+      rep(1:.N, nrow(ppp))
+      # merge
+    ][, n := rowid(release, identity)
+    ][ppp, on = "n"]
+
+  df[,
+     `:=`(
+       ppp_rv = paste0("0", ppp_rv),
+       ppp_av = paste0("0", ppp_av)
+     )][,
+        aux_ver := paste(release, identity, sep = "_")
+     ][,
+       pc_ver := paste(release, ppp_year,
+                       ppp_rv, ppp_av,
+                       identity, sep = "_")]
+
+
+  aux_versions <- df[,unique(aux_ver)]
+  pc_versions  <- df[,unique(pc_ver)]
+
+  ## actual removal of dirs -----------
+  aux_dir <- remove_aux_dir(working_dir  = working_dir,
+                            aux_versions = aux_versions)
+
+  pc_dir <- remove_pc_dir(working_dir  = working_dir,
+                          pc_versions  = pc_versions)
+
+
+  # Update release info ----------
+
+
+  release_info <-
+    save_to_gh(df       = dt,
+               owner    = mt$owner,
+               repo     = mt$repo,
+               branch   = mt$branch,
+               metadata = mt)
+
+  # Returning list ---------
+  # add here the name of the objects that are wanted to be included in the
+  # returning list
+  ret_obj_names  <- c("aux_dir", "pc_dir", "release_info")
+
+
+  lreturn <- vector("list", length = length(ret_obj_names))
+  for (i in seq_along(ret_obj_names)) {
+    lreturn[[i]] <- get(ret_obj_names[i])
+  }
+
+  names(lreturn) <- ret_obj_names
+  return(lreturn)
 
 }
 
-remove_aux_dir <- function() {
+remove_aux_dir <-
+  function(aux_versions, working_dir) {
+
+  wdir        <- fs::path(working_dir, "aux_data")
+  remove_dir(wdir, dirs = aux_versions)
 
 }
 
-remove_pc_dir <- function() {
+remove_pc_dir <-
+  function(pc_versions,
+           working_dir = fs::path(Sys.getenv("PIP_ROOT_DIR"),
+                                  getOption("pipfun.working_dir"))
+  ) {
 
+    wdir        <- fs::path(working_dir, "pc_data")
+    remove_dir(wdir, dirs = pc_versions)
 }
+
+
+#' @rdname create_dir
+#' @export
+remove_dir <- function(wdir, dirs,
+                       verbose = getOption("pipfun.verbose")) {
+  dir_ex <- fs::path(wdir, dirs) |>
+    fs::dir_exists()
+  dir_no_ex <- dir_ex == FALSE
+
+  if (any(dir_no_ex)) {
+    if (verbose) {
+      dir_names <- names(dir_no_ex)
+      cli::cli_alert_info("Folder{?s} {.file {dir_names}} {?does/do} not exist")
+    }
+    return(invisible(dir_names))
+  }
+
+
+  deleted_dir <-
+    names(dir_ex) |>
+    fs::dir_delete() |>
+    {\(.) !fs::dir_exists(.)}()
+
+  return(deleted_dir)
+}
+
+
+
+
+
 
 new_aux_release <- function(measure     = NULL,
                             new_release = format(Sys.Date(), "%Y%m%d"),
@@ -297,7 +462,7 @@ get_latest_pip_release <- function(identity = c("PROD", "INT", "TEST"),
 check_pip_release_inputs <- function(call_args) {
   list2env(call_args, envir = environment())
 
-  if (exists("new_release")){
+  if (exists("new_release", envir = environment(), inherits = FALSE)){
     if (!grepl("[0-9]{8}", new_release))
       cli::cli_abort("{.arg new_release} must be a numeric chracter,
                      representing a date in the form {.field \"%Y%m%d\"}.
