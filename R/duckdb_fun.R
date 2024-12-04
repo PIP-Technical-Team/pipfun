92fd0dc467ae506df041318c2b74109dac846aca
shahronak47 <shahronak47@yahoo.in>
2024-12-04 19:25:17 +0530

    update docs

--- R/duckdb_fun.R
+++ R/duckdb_fun.R
@@ -21,9 +21,18 @@ return_if_exists <- function(country_code, year, poverty_line, con) {
       AND mf.reporting_year = ad.reporting_year
       AND mf.poverty_line = ad.poverty_line
       "))
+  #calculate the arguments that are remaining and return for the pip call
   return(result)
 }
 
+#' Create a dataframe with all possible combinations of `country_code`, `reporting_year` and `poverty_line`
+#'
+#' @param country_code Code of countries to be expanded
+#' @param reporting_year Reported year(s)
+#' @param poverty_line Poverty Line(s)
+#'
+#' @return A dataframe
+#'
 all_args <- function(country_code, reporting_year, poverty_line) {
   expand.grid(country_code = country_code, reporting_year = reporting_year, poverty_line = poverty_line)
 }
