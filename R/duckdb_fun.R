b17442aea7220e30fd483af8e41ee53db48d70cf
shahronak47 <shahronak47@yahoo.in>
2024-12-12 06:36:51 +0530

    change master file to duckdb

--- R/duckdb_fun.R
+++ R/duckdb_fun.R
@@ -3,17 +3,20 @@
 #' @param country_code Country Code
 #' @param year Year
 #' @param poverty_line Poverty Lines
+#' @param con Connection object
 #'
 #' @return Dataframe
 #' @export
 #'
-return_if_exists <- function(country_code, year, poverty_line) {
+return_if_exists <- function(country_code, year, poverty_line, con) {
   all_args_data <- all_args(country_code, year, poverty_line) |>
     duckplyr::as_duckplyr_tibble()
   # This file will be read from shared drive which will be an argument of this function.
   # Additionally there were will more arguments to join instead of only 3
   # In fact, it will be joined by all the arguments in `pip` call
-  master_file <- arrow::read_parquet('master_file.parquet') |>
+  # It is not possible to append to parquet file https://stackoverflow.com/questions/39234391/how-to-append-data-to-an-existing-parquet-file
+  # Writing entire data will be very costly as data keeps on growing, better is to save data in duckdb and append to it.
+  master_file <- DBI::dbGetQuery(con, "select * from master_file") |>
     duckplyr::as_duckplyr_tibble()
 
   args_not_present_in_master <- duckplyr::anti_join(
