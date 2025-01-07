544baddfc3fe8aa9b5df1741a9b22889c23b0aea
RossanaTat <rossana.tatulli@studbocconi.it>
2025-01-07 04:57:55 -0500

    more tests and ensure they pass across sessions

--- tests/testthat/test-save_to_gh.R
+++ tests/testthat/test-save_to_gh.R
@@ -7,6 +7,11 @@ df_sample <- data.frame(
   stringsAsFactors = FALSE
 )
 
+repo <- "aux_test"
+owner <- getOption("pipfun.ghowner")
+creds <- get_github_creds()
+
+
 # Load mockery package
 library(mockery)
 
@@ -32,6 +37,7 @@ test_that("save_to_gh aborts if 'gh' package is not installed", {
 })
 
 test_that("save_to_gh throws an error if metadata is missing 'sha' or 'path'", {
+
   # Case 1: Metadata without 'sha'
   metadata_no_sha <- list(path = "path/to/file.csv")
   expect_error(
@@ -64,11 +70,108 @@ test_that("save_to_gh throws an error if metadata is missing 'sha' or 'path'", {
 })
 
 ## Error catching and metadata retrieval ####
-library(testthat)
-library(mockery)
+# TODO
 
-test_that("save_to_gh handles metadata retrieval correctly", {
+## Save file correctly, 3 cases:
+# 1. new file, new data (data_change is TRUE)
+# 2. old file, new data (data_change is TRUE)
+# 3. old file, old data (data_change is FALSE)
 
+test_that("save_to_gh saves file correctly", {
+
+  # Case 1.
+  res <- save_to_gh(
+    df = df_sample,
+    repo = "aux_test",
+    owner = getOption("pipfun.ghowner"),
+    branch = "DEV",                # Replace with the branch you want to test
+    filename = "new_data_test",     # Replace with a file name that exists in the repo
+    ext = "csv",
+    metadata = NULL,
+    verbose = TRUE
+  )
+
+  res$init |>
+    expect_null() # init should be NULL because file did not exist
+
+  res$data_change |>
+    expect_equal(TRUE)
+
+  # -- delete new file for to prevent subsequent tests call from failing --- #
+  gh::gh(
+    "DELETE /repos/{owner}/{repo}/contents/{path}",
+    owner = owner,
+    repo = repo,
+    path = "new_data_test.csv",
+    message = "delete file for testing",         # Commit message
+    .token = creds$password,
+    sha = res$content$sha,
+    branch = "DEV"                   # Branch where the file exists
+  )
+
+
+  # Case 2.
+  set.seed(Sys.time()) #Ensure randomness across sessions
+
+  res <- save_to_gh(
+    df = data.frame(
+      id = 1:5,
+      value = runif(5, 0, 100),      # Random numeric values between 0 and 100
+      category = sample(letters[1:3], 5, replace = TRUE) # Random categories
+    ),
+    repo = "aux_test",
+    owner = getOption("pipfun.ghowner"),
+    branch = "DEV",                  # Replace with the branch you want to test
+    filename = "test_save",          # Replace with a file name that exists in the repo
+    ext = "csv",
+    metadata = NULL,
+    verbose = TRUE
+  )
+
+  res$init |>
+    is.null() |>
+    expect_false() # init should be available because file existed
+
+  res$data_change |>
+    expect_equal(TRUE)
+
+
+  # Case 3.
+
+  # metadata is available and file exists
+  res <- save_to_gh(
+    df = data.frame(x = 1:5,
+                    y = letters[1:5]),
+    repo = "aux_test",
+    owner = getOption("pipfun.ghowner"),
+    branch = "DEV",                # Replace with the branch you want to test
+    filename = "data_test",     # Replace with a file name that exists in the repo
+    ext = "csv",
+    metadata = NULL,
+    verbose = TRUE
+  )
+
+  res$init |>
+    is.null() |>
+    expect_false() # init should not be NULL because file already existed
+
+  res$init$path |>
+    expect_equal("data_test.csv")
+
+  res$data_change |>
+   expect_equal(FALSE)
+
+
+})
+
+test_that("save_to_gh verbose messages", {
+
+  # File successfully saved
+
+
+  # Data has changed
+
+  # Data has not changed
 
 
 
@@ -76,7 +179,29 @@ test_that("save_to_gh handles metadata retrieval correctly", {
 
 
 
+# check file exists:
+result <- tryCatch(
+  {
+    gh::gh(
+      "GET /repos/{owner}/{repo}/contents/{path}",
+      owner = owner,
+      repo = repo,
+      path = file_path,
+      .params = list(ref = branch),
+      .token = token
+    )
+  },
+  error = function(e) {
+    if (grepl("404", e$message)) {
+      "File does not exist."
+    } else {
+      stop(e) # Re-throw other errors
+    }
+  }
+)
 
+# Check the result
+print(result)
 
 
 
