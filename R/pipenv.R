040b43b9aea4264441883192f91a1ee6d89a61eb
R.Andres Castaneda Aguilar <acastanedaa@worldbank.org>
2025-01-28 18:25:42 -0500

    use rlang instead of base for pipenv

--- R/pipenv.R
+++ R/pipenv.R
@@ -1,36 +1,42 @@
-# Getter function
-#' Title
+# Getter function: Returns the entire .pipenv environment
+#' Get the entire .pipenv environment
 #'
-#' @return
+#' @return The .pipenv environment
 #' @export
 #'
 #' @examples
+#' env <- get_pipenv()
 get_pipenv <- function() {
   .pipenv
 }
 
-#' Get from .pipenv
+# Getter for a specific key from .pipenv
+#' Get a value from .pipenv
 #'
-#' @param key
+#' @param key A character string representing the key
 #'
-#' @return
+#' @return The value associated with the key in .pipenv
 #' @export
 #'
 #' @examples
+#' set_in_pipenv("example_key", 42)
+#' get_from_pipenv("example_key") # returns 42
 get_from_pipenv <- function(key) {
-  get(key, envir = .pipenv)
+  rlang::env_get(.pipenv, key, default = NULL) # Returns NULL if key doesn't exist
 }
 
-# Setter function
-#' Title
+# Setter function: Assign a value in .pipenv
+#' Set a value in .pipenv
 #'
-#' @param key
-#' @param value
+#' @param key A character string representing the key
+#' @param value The value to store in .pipenv
 #'
-#' @return
+#' @return The assigned value (invisibly)
 #' @export
 #'
 #' @examples
+#' set_in_pipenv("example_key", 42)
 set_in_pipenv <- function(key, value) {
-  assign(key, value, envir = .pipenv)
+  rlang::env_poke(.pipenv, key, value)
+  invisible(value)  # Return value invisibly to avoid clutter in console
 }
