b05a7fe371a375ad6d795c5a142632eead7d7a6a
R.Andres Castaneda <acastanedaa@worldbank.org>
2025-04-11 15:22:01 -0400

    init print.piplog S3 method function... not working well yet

--- R/log_helpers.R
+++ R/log_helpers.R
@@ -36,3 +36,18 @@ log_warn <- function(message, ...,
 }
 
 
+#' @export
+print.piplog <- function(x, ...) {
+  cli::cli_h2("Log entries:")
+  if (nrow(x) == 0) {
+    cli::cli_alert_info("The log is empty.")
+    return(invisible(x))
+  }
+  for (i in seq_len(nrow(x))) {
+    cli::cli_alert("{.strong [{x$time[i]}]} {.emph {toupper(x$event[i])}} — {.code {x$message[i]}}")
+    cli::cli_text("Function: {.code {x$fun[i]}} (from {x$package[i]})")
+    if (!is.null(x$trace[[i]])) cli::cli_text("• Trace: {deparse(x$trace[[i]])}")
+    cli::cli_text("")
+  }
+  invisible(x)
+}
