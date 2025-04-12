c3b964177770091bf24c652124aba642147683aa
R.Andres Castaneda <acastanedaa@worldbank.org>
2025-04-12 07:56:45 -0400

    fix tests to use logmeta

--- tests/testthat/test-log.R
+++ tests/testthat/test-log.R
@@ -83,11 +83,11 @@ test_that("log_add fallback to sys.call when .trace is NULL", {
 })
 
 
-### capturing dots -----------
-test_that("log_add() captures dots passed to log_info()", {
+### capturing logmeta -----------
+test_that("log_add() captures logmeta passed to log_info()", {
   log_init("dotstest", overwrite = TRUE)
 
-  log_info("Testing dots", name = "dotstest", a = 1:3, b = "hello")
+  log_info("Testing dots", name = "dotstest", logmeta = list(a = 1:3, b = "hello"))
   log <- log_get("dotstest")
   args <- log$args[[1]]
 
@@ -95,10 +95,12 @@ test_that("log_add() captures dots passed to log_info()", {
   expect_equal(args$b, "hello")
 })
 
-test_that("log_add() captures dots passed directly", {
+test_that("log_add() captures logmeta passed directly", {
   log_init("directdots", overwrite = TRUE)
 
-  log_add("info", "Direct dots test", name = "directdots", x = 99, y = "yay")
+  log_add("info", "Direct dots test", name = "directdots",
+          logmeta = list(x = 99, y = "yay"))
+
   log <- log_get("directdots")
   args <- log$args[[1]]
 
@@ -109,7 +111,9 @@ test_that("log_add() captures dots passed directly", {
 test_that("log_add() respects args override", {
   log_init("explicitargs", overwrite = TRUE)
 
-  log_add("info", "Explicit args test", name = "explicitargs", args = list(z = "onlythis"))
+  log_add("info", "Explicit args test", name = "explicitargs",
+          args = list(z = "onlythis"))
+
   log <- log_get("explicitargs")
   args <- log$args[[1]]
 
@@ -117,11 +121,11 @@ test_that("log_add() respects args override", {
   expect_equal(args$z, "onlythis")
 })
 
-test_that("log_add() merges all captured arguments correctly", {
+test_that("log_add() merges all captured arguments and logmeta correctly", {
   log_init("mergeargs", overwrite = TRUE)
 
   test_fun <- function(a = 1, b = 2, ...) {
-    log_info("Merged", name = "mergeargs", ...)
+    log_info("Merged", name = "mergeargs", logmeta = list(...))
   }
 
   test_fun(x = "hello", y = TRUE)
@@ -137,60 +141,58 @@ test_that("log_add() merges all captured arguments correctly", {
 
 
 
-
-
 # helpers ------------
-test_that("log_error captures message and calling arguments", {
-  log_init("testlog", overwrite = TRUE)
+test_that("log_info() captures arguments from parent function", {
+  log_init("loginfo_test", overwrite = TRUE)
 
-  simulate_error <- function(a = 42, b = "oops") {
-    log_error("This is an error", name = "testlog")
+  my_function <- function(a = 1, b = 2, ...) {
+    log_info("Info from my_function", name = "loginfo_test")
   }
 
-  simulate_error()
-  log <- log_get("testlog")
+  my_function(x = 42)
 
-  expect_equal(nrow(log), 1)
-  expect_equal(log$event[[1]], "error")
-  expect_match(log$message[[1]], "This is an error")
-  expect_equal(log$args[[1]]$a, 42)
-  expect_equal(log$args[[1]]$b, "oops")
+  log <- log_get("loginfo_test")
+  args <- log$args[[1]]
+
+  expect_true("a" %in% names(args))
+  expect_true("b" %in% names(args))
+  expect_true("x" %in% names(args))
+  expect_equal(args$x, 42)
 })
 
-test_that("log_warn captures arguments correctly", {
-  log_init("testlog", overwrite = TRUE)
+test_that("log_warn() captures arguments and logs warning", {
+  log_init("logwarn_test", overwrite = TRUE)
 
-  simulate_warn <- function(x = TRUE) {
-    log_warn("This is a warning", name = "testlog")
+  my_warn <- function(y = 10, ...) {
+    log_warn("Warning issued", name = "logwarn_test")
   }
 
-  simulate_warn()
-  log <- log_get("testlog")
+  my_warn(extra = TRUE)
+  log <- log_get("logwarn_test")
+  args <- log$args[[1]]
 
-  expect_equal(nrow(log), 1)
   expect_equal(log$event[[1]], "warning")
-  expect_match(log$message[[1]], "This is a warning")
-  expect_equal(log$args[[1]]$x, TRUE)
+  expect_true("y" %in% names(args))
+  expect_true("extra" %in% names(args))
 })
 
-test_that("log_info captures output and arguments", {
-  log_init("testlog", overwrite = TRUE)
+test_that("log_error() captures environment and logs error", {
+  log_init("logerror_test", overwrite = TRUE)
 
-  simulate_info <- function(vec = 1:3) {
-    result <- sum(vec)
-    log_info("Logging info with result", name = "testlog", output = result)
+  my_error <- function(code = "A") {
+    log_error("Error encountered", name = "logerror_test")
   }
 
-  simulate_info()
-  log <- log_get("testlog")
+  my_error()
+  log <- log_get("logerror_test")
+  args <- log$args[[1]]
 
-  expect_equal(nrow(log), 1)
-  expect_equal(log$event[[1]], "info")
-  expect_equal(log$args[[1]]$vec, 1:3)
-  expect_equal(log$output[[1]], 6)
+  expect_equal(log$event[[1]], "error")
+  expect_equal(args$code, "A")
 })
 
 
+## print --------------
 test_that("print.piplog produces output without error", {
   skip() # we need to test when print.piplog is finished.
   msg <- "Printing test"
